586cb8e74ef7d470fcc4f3ec7a56812ccc095681
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-03-15 06:27:42 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from './ProfileCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ProfileCard title="Omar" />
+      <ProfileCard title="Mariam" />
+      <ProfileCard title="Yosuf" />
     </div>
   );
 }
---
+++ src/ProfileCard.js
@@ -0,0 +1,9 @@
+function ProfileCard({ title }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+    </div>
+  );
+}
+
+export default ProfileCard;
