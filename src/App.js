8edd6b2f0ad2233c5a39a87c80e3ce67341b6d43
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-03-15 21:13:46 +0200

    Adde pda logos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
   return (
     <div>
-      <ProfileCard title="Omar" />
-      <ProfileCard title="Mariam" />
-      <ProfileCard title="Yosuf" />
+      <ProfileCard title="Omar" image={AlexaImage} />
+      <ProfileCard title="Mariam" image={CortImage} />
+      <ProfileCard title="Yosuf" image={SiriImage} />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
-function ProfileCard({ title }) {
+function ProfileCard({ title, image }) {
   return (
     <div>
+      <img src={image} alt="pda logo" />
       <h3>{title}</h3>
     </div>
   );
