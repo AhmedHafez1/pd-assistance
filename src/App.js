b979fa9b434a177f88cb1a255fc9b70bab23b1af
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-03-17 09:21:26 +0200

    cards styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import ProfileCard from './ProfileCard';
-import AlexaImage from './images/alexa.png';
-import CortImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
-    <div>
-      <ProfileCard title="Omar" image={AlexaImage} />
-      <ProfileCard title="Mariam" image={CortImage} />
-      <ProfileCard title="Yosuf" image={SiriImage} />
+    <div className="container">
+      <h2 className="py-3 px-6 my-6 has-background-primary has-text-primary-light is-size-2">
+        Welcome to PDS
+      </h2>
+      <div className="columns">
+        <div className="column">
+          <ProfileCard
+            title="Omar"
+            handle="@Ackkked"
+            image={AlexaImage}
+            description="Creative writer with a passion for storytelling and a vivid imagination."
+          />
+        </div>
+        <div className="column">
+          <ProfileCard
+            title="Mariam"
+            handle="@Ioetdwse"
+            image={CortImage}
+            description="Results-driven marketer with experience in strategy, analytics, and campaign execution."
+          />
+        </div>
+        <div className="column">
+          <ProfileCard
+            title="Yosuf"
+            handle="@Mrwsyarw"
+            image={SiriImage}
+            description="Dedicated educator committed to fostering a love of learning in students."
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,22 @@
-function ProfileCard({ title, image }) {
+function ProfileCard({ title, image, handle, description }) {
   return (
     <div>
-      <img src={image} alt="pda logo" />
-      <h3>{title}</h3>
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src={image} alt="pda logo" />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">{title}</p>
+              <p class="subtitle is-6">{handle}</p>
+            </div>
+          </div>
+          <div class="content">{description}</div>
+        </div>
+      </div>
     </div>
   );
 }
